8043adcce64225b0e897e89bb565772b62c11098
Sergey Darvin <srg.darvin@gmail.com>
2023-10-05 21:09:27 +0300

    completed the Task0

---
+++ src/main/java/edu/hw1/Task0.java
@@ -0,0 +1,11 @@
+package edu.hw1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Task0 {
+    public final static Logger LOGGER = LogManager.getLogger();
+    public static void main(String[] args) {
+        LOGGER.info("Hello, World!");
+    }
+}
