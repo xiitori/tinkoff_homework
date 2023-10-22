b3e2431266d964359c882c71469757ba39e92596
Sergey Darvin <srg.darvin@gmail.com>
2023-10-22 22:54:02 +0300

    add tests for task3

---
+++ src/test/java/edu/hw2/Task3Test.java
@@ -0,0 +1,28 @@
+package edu.hw2;
+
+import edu.hw2.task3.ConnectionException;
+import edu.hw2.task3.DefaultConnectionManager;
+import edu.hw2.task3.FaultyConnectionManager;
+import edu.hw2.task3.PopularCommandExecutor;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Task3Test {
+    @Test
+    void faultyConnectionManagerTest() {
+        PopularCommandExecutor executor = new PopularCommandExecutor(new FaultyConnectionManager(), 3);
+        assertThrows(ConnectionException.class, executor::updatePackages);
+    }
+
+    @Test
+    void defaultConnectionManagerTest() {
+        PopularCommandExecutor executor = new PopularCommandExecutor(new DefaultConnectionManager(), 1);
+        assertDoesNotThrow(executor::updatePackages);
+        assertDoesNotThrow(executor::updatePackages);
+        assertDoesNotThrow(executor::updatePackages);
+        assertDoesNotThrow(executor::updatePackages);
+        assertDoesNotThrow(executor::updatePackages);
+        assertThrows(ConnectionException.class, executor::updatePackages);
+    }
+}
