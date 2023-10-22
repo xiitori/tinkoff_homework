6a2cdde21812bb29c3350fdac42254c78ff22b03
Sergey Darvin <srg.darvin@gmail.com>
2023-10-22 22:06:06 +0300

    complete task2

--- src/main/java/edu/hw2/task2/Rectangle.java
+++ src/main/java/edu/hw2/task2/Rectangle.java
@@ -4,12 +4,25 @@ public class Rectangle {
     private int width;
     private int height;
 
-    public void setWidth(int width) {
+    public Rectangle(int width, int height) {
         this.width = width;
+        this.height = height;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
+    public Rectangle setWidth(int width) {
+        if (width == height) {
+            return new Square(width);
+        } else {
+            return new Rectangle(width, height);
+        }
+    }
+
+    public Rectangle setHeight(int height) {
+        if (height == width) {
+            return new Square(height);
+        } else {
+            return new Rectangle(width, height);
+        }
     }
 
     public double area() {
--- src/main/java/edu/hw2/task2/Square.java
+++ src/main/java/edu/hw2/task2/Square.java
@@ -1,15 +1,7 @@
 package edu.hw2.task2;
 
 public class Square extends Rectangle {
-    @Override
-    public void setWidth(int width) {
-        super.setHeight(width);
-        super.setWidth(width);
-    }
-
-    @Override
-    public void setHeight(int height) {
-        super.setHeight(height);
-        super.setWidth(height);
+    public Square(int size) {
+        super(size, size);
     }
 }
