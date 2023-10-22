5c140f111fc2bb46556e48206ed59f3c6ad177cd
Sergey Darvin <srg.darvin@gmail.com>
2023-10-22 23:09:22 +0300

    fix checkstyle

--- src/main/java/edu/hw2/task4/MethodCaller.java
+++ src/main/java/edu/hw2/task4/MethodCaller.java
@@ -1,6 +1,11 @@
 package edu.hw2.task4;
 
 public class MethodCaller {
+
+    private MethodCaller() {
+
+    }
+
     public static CallingInfo callMethod() {
         return CallingInfo.callingInfo();
     }
