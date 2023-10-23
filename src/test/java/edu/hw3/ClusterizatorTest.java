f5faf567564e88846b2573c6cf24d54b995f809f
Sergey Darvin <srg.darvin@gmail.com>
2023-10-23 22:17:24 +0300

    add tests for task2

--- src/test/java/edu/hw3/ClusterizatorTest.java
+++ src/test/java/edu/hw3/ClusterizatorTest.java
@@ -4,13 +4,43 @@ import edu.hw3.task2.Clusterizator;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ClusterizatorTest {
     @Test
-    void clusterizeTest() {
+    void correctBraceTest() {
         List<String> result = Clusterizator.clusterize("()()()");
         for (String brackets : result) {
             assertThat(brackets).isEqualTo("()");
         }
+
+        result = Clusterizator.clusterize("((()))");
+        assertThat(result.get(0)).isEqualTo("((()))");
+
+        result = Clusterizator.clusterize("((()))(())()()(()())");
+        assertThat(result.get(0)).isEqualTo("((()))");
+        assertThat(result.get(1)).isEqualTo("(())");
+        assertThat(result.get(2)).isEqualTo("()");
+        assertThat(result.get(3)).isEqualTo("()");
+        assertThat(result.get(4)).isEqualTo("(()())");
+
+        result = Clusterizator.clusterize("((())())(()(()()))");
+        assertThat(result.get(0)).isEqualTo("((())())");
+        assertThat(result.get(1)).isEqualTo("(()(()()))");
+    }
+
+    @Test
+    void illegalCharsTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Clusterizator.clusterize("((()()))()J()()()");
+        });
+    }
+
+    @Test
+    void notBalancedBraceStringTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Clusterizator.clusterize("((())(())(()(()()))((()))");
+        });
+
     }
 }
