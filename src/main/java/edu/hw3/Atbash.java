6d9387f240906d1dbeef6fc0e3d13ba7eade2b95
Sergey Darvin <srg.darvin@gmail.com>
2023-10-23 21:43:01 +0300

    complete task1

--- src/main/java/edu/hw3/Atbash.java
+++ src/main/java/edu/hw3/Atbash.java
@@ -2,9 +2,38 @@ package edu.hw3;
 
 public class Atbash {
 
-    private static final char[] Dictionary = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y' , 'z'};
+    private Atbash() {
+
+    }
+
+    private static final char[] DICTIONARY = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
     public static String atbash(String string) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < string.length(); i++) {
+            char letter = string.charAt(i);
+            stringBuilder.append(getAtbashLetter(letter));
+        }
+        return stringBuilder.toString();
+    }
+
+    private static char getAtbashLetter(char letter) {
+        boolean isUpperCase = false;
+        char findLetter;
+        if (Character.isUpperCase(letter)) {
+            isUpperCase = true;
+            findLetter = Character.toLowerCase(letter);
+        } else {
+            findLetter = letter;
+        }
+
+        for (int i = 0; i < DICTIONARY.length; i++) {
+            if (DICTIONARY[i] == findLetter) {
+                return isUpperCase ? Character.toUpperCase(DICTIONARY[DICTIONARY.length - 1 - i])
+                    : DICTIONARY[DICTIONARY.length - 1 - i];
+            }
+        }
+        return letter;
     }
 }
