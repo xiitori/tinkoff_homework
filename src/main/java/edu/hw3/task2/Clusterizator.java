aef2f408d4245d85531a7fbcd9582fd87d3f90b0
Sergey Darvin <srg.darvin@gmail.com>
2023-10-23 21:50:48 +0300

    add class Clusterizator

---
+++ src/main/java/edu/hw3/task2/Clusterizator.java
@@ -0,0 +1,14 @@
+package edu.hw3.task2;
+
+import java.util.List;
+
+public class Clusterizator {
+
+    private Clusterizator() {
+
+    }
+
+    public static List<String> clusterize(String braceString) {
+
+    }
+}
