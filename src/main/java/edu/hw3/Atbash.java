211f318bee180e5a26971a304983a7fac5e18b77
Sergey Darvin <srg.darvin@gmail.com>
2023-10-23 20:58:01 +0300

    create atbash class and initialize dictionary of chars

---
+++ src/main/java/edu/hw3/Atbash.java
@@ -0,0 +1,10 @@
+package edu.hw3;
+
+public class Atbash {
+
+    private static final char[] Dictionary = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y' , 'z'};
+    public static String atbash(String string) {
+        return null;
+    }
+}
