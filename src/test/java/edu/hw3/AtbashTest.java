e6b6e772c143c3ec90976df5fce9132e950c88eb
Sergey Darvin <srg.darvin@gmail.com>
2023-10-23 21:43:16 +0300

    add tests for task1

--- src/test/java/edu/hw3/AtbashTest.java
+++ src/test/java/edu/hw3/AtbashTest.java
@@ -1,12 +1,33 @@
 package edu.hw3;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AtbashTest {
     @Test
-    void atbashTest() {
-        String result = Atbash.atbash("Hello World!");
+    void helloWorldTest() {
+        String result = Atbash.atbash("Hello world!");
         assertThat(result).isEqualTo("Svool dliow!");
+    }
+
+    @Test
+    void notLettersTest() {
+        String result = Atbash.atbash("{}}[][!@#$!");
+        assertThat(result).isEqualTo("{}}[][!@#$!");
+    }
+
+    @Test
+    void russianLettersTest() {
+        String result = Atbash.atbash("Привет, мир!");
+        assertThat(result).isEqualTo("Привет, мир!");
+    }
+
+    @Test
+
+    void emptyStringTest() {
+        String result = Atbash.atbash("");
+        assertThat(result).isEqualTo("");
     }
 }
