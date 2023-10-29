5cdf9a3c7bb77c9147219835e85c88b3618f845a
Sergey Darvin <srg.darvin@gmail.com>
2023-10-29 23:37:31 +0300

    corrected the declaration of the variables

--- src/main/java/edu/hw2/task3/FaultyConnection.java
+++ src/main/java/edu/hw2/task3/FaultyConnection.java
@@ -5,11 +5,11 @@ import org.apache.logging.log4j.Logger;
 
 public class FaultyConnection implements Connection {
 
-    private final static Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     private static int exceptionCounter = 0;
 
-    private final static int EXCEPTION_FREQUENCY = 3;
+    private static final int EXCEPTION_FREQUENCY = 3;
 
     @Override
     public void execute(String command) {
--- src/main/java/edu/hw2/task3/StableConnection.java
+++ src/main/java/edu/hw2/task3/StableConnection.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 public class StableConnection implements Connection {
 
-    private final static Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Override
     public void execute(String command) {
