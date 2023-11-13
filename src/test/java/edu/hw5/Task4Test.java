31ad9c3dca00ed6ec59ef39af99674a07a882e98
Sergey Darvin <srg.darvin@gmail.com>
2023-11-13 17:01:29 +0300

    competed tests for task4

---
+++ src/test/java/edu/hw5/Task4Test.java
@@ -0,0 +1,32 @@
+package edu.hw5;
+
+import edu.hw5.task4.PasswordValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task4Test {
+
+    @Test
+    @DisplayName("Тест валидных данных")
+    void validatePasswordTest() {
+        String correctPassword = "qwerty123#";
+        String incorrectPassword = "fdjbbfnljbveq";
+
+        boolean firstAnswer = PasswordValidator.validate(correctPassword);
+        boolean secondAnswer = PasswordValidator.validate(incorrectPassword);
+
+        assertThat(firstAnswer).isTrue();
+        assertThat(secondAnswer).isFalse();
+    }
+
+    @Test
+    @DisplayName("Тест пустой строки")
+    void emptyPasswordTest() {
+        String password = "";
+
+        boolean answer = PasswordValidator.validate(password);
+
+        assertThat(answer).isFalse();
+    }
+}
