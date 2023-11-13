1ff9697a97145feeaab564a3ab5d625ac2756b86
Sergey Darvin <srg.darvin@gmail.com>
2023-11-13 19:56:50 +0300

    completed task5

--- src/main/java/edu/hw5/task5/NumberPlateValidator.java
+++ src/main/java/edu/hw5/task5/NumberPlateValidator.java
@@ -4,13 +4,14 @@ import java.util.regex.Pattern;
 
 public class NumberPlateValidator {
 
-    private static final Pattern numberPlatePattern = Pattern.compile("^[АВЕКМНОРСТУХ](\\d{3})(?<!000)[АВЕКМНОРСТУХ]{2}(\\d{2,3})$");
+    private static final Pattern NUMBER_PLATE_PATTERN = Pattern.compile(
+        "^[АВЕКМНОРСТУХ](\\d{3})(?<!000)[АВЕКМНОРСТУХ]{2}(\\d{2,3})$");
 
     private NumberPlateValidator() {
 
     }
 
     public static boolean validate(String numberPlate) {
-        return numberPlate.matches(numberPlatePattern.pattern());
+        return numberPlate.matches(NUMBER_PLATE_PATTERN.pattern());
     }
 }
