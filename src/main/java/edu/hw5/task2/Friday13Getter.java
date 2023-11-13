ac6621229566536bfe862a8501ac408305fe03b2
Sergey Darvin <srg.darvin@gmail.com>
2023-11-13 21:03:01 +0300

    completed task2

---
+++ src/main/java/edu/hw5/task2/Friday13Getter.java
@@ -0,0 +1,39 @@
+package edu.hw5.task2;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Friday13Getter {
+    private static final int THIRTEENTH = 13;
+
+    private int year;
+
+    public Friday13Getter(int year) {
+        setYear(year);
+    }
+
+    public void setYear(int year) {
+        if (year > 0) {
+            this.year = year;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<LocalDate> getFridays13() {
+        LocalDate currentDate = LocalDate.of(year, Month.JANUARY, THIRTEENTH);
+        var list = new LinkedList<LocalDate>();
+
+        while (currentDate.getYear() == year) {
+            DayOfWeek month = currentDate.getDayOfWeek();
+            if (month == DayOfWeek.FRIDAY) {
+                list.add(currentDate);
+            }
+            currentDate = currentDate.plusMonths(1);
+        }
+        return list;
+    }
+}
