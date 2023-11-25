08b648f062a334215ad623a7adb0634f43d9ddea
Sergey Darvin <srg.darvin@gmail.com>
2023-11-25 16:48:56 +0300

    completed task6

--- src/main/java/edu/hw6/task6/PortScanner.java
+++ src/main/java/edu/hw6/task6/PortScanner.java
@@ -7,27 +7,58 @@ import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.SocketException;
 import java.util.HashMap;
+import java.util.List;
 
 public class PortScanner {
-
     private static final HashMap<Integer, String> PORTS_INFO = new HashMap<>();
 
     private static final Logger LOGGER = LogManager.getLogger();
 
+    private static final String FORMAT = "|%-10s|  %-10s|  %-20s|";
+
+    private static final String NA = "N/A";
+
+    static {
+        PORTS_INFO.put(135, "EPMAP");
+        PORTS_INFO.put(137, "NETBIOS-NS");
+        PORTS_INFO.put(138, "NETBIOS-DGM");
+        PORTS_INFO.put(139, "NETBIOS-SSN");
+        PORTS_INFO.put(445, "MICROSOFT-DS");
+        PORTS_INFO.put(1900, "MICROSOFT SSDP");
+        PORTS_INFO.put(3702, "WS-Discovery");
+        PORTS_INFO.put(5050, "Yahoo! Messenger");
+        PORTS_INFO.put(5353, "Milticast DNS");
+        PORTS_INFO.put(5432, "PostgreSQL");
+        PORTS_INFO.put(5355, "LLMNR");
+
+    }
+
     public void printPortsInfo(int from, int to) {
-        LOGGER.info("Протокол  Порт");
+        LOGGER.info(String.format(FORMAT, "Протокол", "Порт", "Сервис"));
         for (int i = from; i < to; i++) {
-            try (ServerSocket socket = new ServerSocket(i)) {
-
-            } catch (IOException e) {
-                LOGGER.info("TCP" + "       " + i);
+            String portInfo = PORTS_INFO.getOrDefault(i, NA);
+            if (isTCPPortClosed(i)) {
+                LOGGER.info(String.format(FORMAT, "TCP", i, portInfo));
             }
-
-            try (DatagramSocket socket = new DatagramSocket(i)) {
-
-            } catch (SocketException e) {
-                LOGGER.info("UDP" + "       " + i);
+            if (isUDPPortClosed(i)) {
+                LOGGER.info(String.format(FORMAT, "UDP", i, portInfo));
             }
+        }
+    }
+
+    private boolean isTCPPortClosed(int port) {
+        try (ServerSocket ignored = new ServerSocket(port)) {
+            return false;
+        } catch (IOException e) {
+            return true;
+        }
+    }
+
+    private boolean isUDPPortClosed(int port) {
+        try (DatagramSocket ignored = new DatagramSocket(port)) {
+            return false;
+        } catch (SocketException e) {
+            return true;
         }
     }
 }
